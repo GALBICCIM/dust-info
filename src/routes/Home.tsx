40b671670db89b13f227c51e0ec52766f0a3469a
GALBICCIM <godvictory0909@gmail.com>
2025-05-19 13:49:06 +0900

    feat: Select date and info implementation

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,14 +1,44 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Home: React.FC = () => {
+	const [date, setDate] = useState<string>("");
+	const [selectedInfo, setSelectedInfo] = useState<string>("");
+
+	const onChangeDate = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setDate(event.target.value);
+	};
+
+	const onChangeInfo = (event: React.ChangeEvent<HTMLSelectElement>) => {
+		setSelectedInfo(event.target.value);
+	};
+
 	return (
-		<>
+		<div>
 			<h1>메인 페이지입니다.</h1>
 
-			<Link to={"/pm10"}>미세먼지 정보 보러가기</Link>
-			<Link to={"/pm25"}>초미세먼지 정보 보러가기</Link>
-			<Link to={"/ozone"}>오존 정보 보러가기</Link>
-		</>
+			<div>
+				<label>
+					날짜 선택
+					<input type="date" value={date} onChange={onChangeDate} />
+				</label>
+				<br />
+				<label>
+					정보 선택
+					<select value={selectedInfo} onChange={onChangeInfo}>
+						<option value="">---조회할 정보를 선택하세요.---</option>
+						<option value="PM10">미세먼지</option>
+						<option value="PM25">초미세먼지</option>
+						<option value="O3">오존</option>
+					</select>
+				</label>
+			</div>
+
+			<h2>선택된 날짜 : {date || "없음"}</h2>
+			<h2>선택된 정보 : {selectedInfo || "없음"}</h2>
+
+			<Link to={selectedInfo === "O3" ? "ozone" : selectedInfo.toLowerCase()}>정보 보러가기</Link>
+		</div>
 	);
 };
 
