bb42a7d627bd9d616bcb8d173ec64a31c1182c6a
GALBICCIM <godvictory0909@gmail.com>
2025-05-21 08:21:40 +0900

    docs: Add comment for getInfoLabel function

--- src/utils/getInfoLabel.ts
+++ src/utils/getInfoLabel.ts
@@ -1,3 +1,8 @@
+/**
+ * informCode를 한글 정보로 변환하는 함수입니다.
+ * @param code informCode { ex) 'PM10', 'PM25', 'O3' }
+ * @returns informCode를 한글로 표현한 문자열
+ */
 export const getInfoLabel = (code: string): string => {
 	switch (code) {
 		case "PM10":
