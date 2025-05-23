6b8b421a5ab1e61ccd0e9268fb13dcb96661655e
GALBICCIM <godvictory0909@gmail.com>
2025-05-23 23:03:36 +0900

    style: Add reset css

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import { Home, Info } from "routes";
+
+import GlobalStyle from "styles/GlobalStyle";
 
 const App: React.FC = () => {
 	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/info" element={<Info />} />
-			</Routes>
-		</Router>
+		<>
+			<GlobalStyle />
+			<Router>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/info" element={<Info />} />
+				</Routes>
+			</Router>
+		</>
 	);
 };
 
