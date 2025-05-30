cfca05341ffcd59d2377a214c6ee2f52a3a4dc55
GALBICCIM <godvictory0909@gmail.com>
2025-05-30 23:23:55 +0900

    chore: modify GoBack component

--- src/components/GoBack.tsx
+++ src/components/GoBack.tsx
@@ -1,5 +1,7 @@
+import React from "react";
+
 import { Link } from "react-router-dom";
 
-const GoBack = () => <Link to={"/"}>홈으로 돌아가기</Link>;
+const GoBack: React.FC = () => <Link to={"/"}>홈으로 돌아가기</Link>;
 
 export default GoBack;
