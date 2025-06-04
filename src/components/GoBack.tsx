b9ec509a71a78370d5853f8d0ae7d2d6a75ee104
GALBICCIM <godvictory0909@gmail.com>
2025-06-04 10:01:27 +0900

    style(GoBack.tsx): Styling GoBack component

--- src/components/GoBack.tsx
+++ src/components/GoBack.tsx
@@ -2,6 +2,14 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
-const GoBack: React.FC = () => <Link to={"/"}>홈으로 돌아가기</Link>;
+import { Button, Text } from "styles/GlobalStyle";
+
+const GoBack: React.FC = () => (
+	<Link to={"/"}>
+		<Button width="9vw" height="5vh">
+			<Text>홈으로 돌아가기</Text>
+		</Button>
+	</Link>
+);
 
 export default GoBack;
